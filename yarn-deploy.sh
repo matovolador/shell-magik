9c267a18b81ea5e75d8bfec992cc9527144b5392
matias garafoni <mgarafoni@clearrtb.com>
2019-06-04 12:11:48 -0300

    added readme to the yarn deploy script

--- yarn-deploy.sh
+++ yarn-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# So this assumes you dont actually have a shell script located on your project base path. so it goes back a step, and executes the yarn command. OFC...you need to change the user, host, and maybe even the destination path? (doubtfull)
 cd ..
 yarn run build
 scp -r dist/* root@172.105.0.231:/var/www/html
