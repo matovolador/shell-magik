fe0314ca1ce00d19b4db6f8d734cd43e0b0fcd21
matias garafoni <mgarafoni@clearrtb.com>
2019-06-10 12:11:28 -0300

    added very important info that could save your life

--- yarn-deploy.sh
+++ yarn-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Put this script on project root. If you need to use credentials, store them somewhere else and give them as variables to the script.
+# Put this script on project root. If you need to use credentials, store them somewhere else and give them as variables to the script. PLEASE DONT PUT THIS ON A BRANCH THAT AINT MASTER OR SUFFER THE CONSECUENCES.
 yarn run build && scp -r dist/* root@172.105.0.231:/var/www/html || echo "Build failed. Skipping deployment."
 read -p "Enter any key to exit." continue
